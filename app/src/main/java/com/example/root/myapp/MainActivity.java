5fa5e4021a89206771afc815f7050956b6c0697b
Alok Singh <singhalok641@gmail.com>
2017-10-28 10:27:53 +0530

    Implemented LMBluetooth Sdk

--- app/src/main/java/com/example/root/myapp/MainActivity.java
+++ app/src/main/java/com/example/root/myapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.root.myapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.root.myapp.BleActivity;
+import com.example.root.myapp.ClassicBluetoothActivity;
+import com.example.root.myapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,10 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCbt = (Button) findViewById(R.id.btn_cbt_activity);
+        Button btnBle = (Button) findViewById(R.id.btn_ble_activity);
+
+        btnCbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ClassicBluetoothActivity.class));
+            }
+        });
+
+        btnBle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BleActivity.class));
+            }
+        });
     }
-
-
 }
-
-
 
